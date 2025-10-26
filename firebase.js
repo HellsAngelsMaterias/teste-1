16af95f5a165adc279ad3860660d3e1a747cca9d
HellsAngelsMaterias <jonelukinhas43@gmail.com>
2025-10-26 02:54:00 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,44 +1,23 @@
 /* ===================================================
  * firebase.js
- * Responsável por inicializar o Firebase
- * e exportar as dependências (auth, db) e funções.
+ * Módulo central do Firebase.
+ * Inicializa o app e exporta todas as funções
+ * de Auth e Database necessárias.
  * =================================================== */
 
+// --- IMPORTS PRINCIPAIS ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase, ref, set, push, onValue, remove, get, query, orderByChild, equalTo, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCPNd9INqIfqG1-rjaYAlz988RLDZvL528", 
-  authDomain: "hells-angels-438c2.firebaseapp.com",
-  databaseURL: "https://hells-angels-438c2-default-rtdb.firebaseio.com/",
-  projectId: "hells-angels-438c2",
-  storageBucket: "hells-angels-438c2.firebasestorage.app",
-  messagingSenderId: "429406215315",
-  appId: "1:429406215315:web:96b68b172247824b308166",
-  measurementId: "G-CR415MEY32"
-};
-
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-// Exportar instâncias principais
-export { auth, db };
-
-// Exportar funções do Auth para não precisar importar em todo arquivo
-export { 
+import { 
+    getAuth, 
     sendPasswordResetEmail, 
     signInWithEmailAndPassword, 
     createUserWithEmailAndPassword, 
     updateProfile, 
     onAuthStateChanged, 
     signOut 
-};
-
-// Exportar funções do Database para não precisar importar em todo arquivo
-export { 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { 
+    getDatabase, 
     ref, 
     set, 
     push, 
@@ -48,5 +27,54 @@ export {
     query, 
     orderByChild, 
     equalTo, 
-    update 
+    update,
+    onDisconnect,     // <-- ADICIONADO AQUI
+    serverTimestamp   // <-- ADICIONADO AQUI
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+// --- CONFIGURAÇÃO ---
+const firebaseConfig = {
+  // Substitua pelos seus dados
+  apiKey: "AIzaSyCPNd9INqIfqG1-rjaYAlz988RLDZvL528", 
+  authDomain: "hells-angels-438c2.firebaseapp.com",
+  databaseURL: "https://hells-angels-438c2-default-rtdb.firebaseio.com/",
+  projectId: "hells-angels-438c2",
+  storageBucket: "hells-angels-438c2.firebasestorage.app",
+  messagingSenderId: "429406215315",
+  appId: "1:429406215315:web:96b68b172247824b3080e7"
+};
+
+// --- INICIALIZAÇÃO ---
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// --- EXPORTS ---
+// Exporta tudo para que os outros módulos possam usar
+export {
+    app,
+    auth,
+    db,
+    
+    // Funções de Auth
+    sendPasswordResetEmail,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    updateProfile,
+    onAuthStateChanged,
+    signOut,
+    
+    // Funções de Database
+    ref,
+    set,
+    push,
+    onValue,
+    remove,
+    get,
+    query,
+    orderByChild,
+    equalTo,
+    update,
+    onDisconnect,     // <-- ADICIONADO AQUI
+    serverTimestamp   // <-- ADICIONADO AQUI
 };
